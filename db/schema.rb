be8bbef42fc76eedcb052c79dc50c48ccec93a9c
zhaozongchang <xingyingsuifeng@gmail.com>
2019-06-06 22:20:08 +0800

    实作项目的CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190606122033) do
+ActiveRecord::Schema.define(version: 20190606123241) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20190606122033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "products", force: :cascade do |t|
