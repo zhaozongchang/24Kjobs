1093cb3d8650df20a0a967520f14ad819573e0d7
zhaozongchang <xingyingsuifeng@gmail.com>
2019-06-07 09:17:48 +0800

    实作admin的CRUD

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,41 @@
+class Admin::JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.save
+    redirect_to admin_jobs_path
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to admin_jobs_path
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end 
+end
