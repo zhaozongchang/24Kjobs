82a57e036166aa562bf2e981eb415eebdd8f2dbf
zhaozongchang <xingyingsuifeng@gmail.com>
2019-06-10 17:11:58 +0800

    实作devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190610012051) do
+ActiveRecord::Schema.define(version: 20190610024432) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20190610012051) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
